5127f9d0ee98e51ebedab3ba98aec2fae0eac110
shoprev <admin@shoprev.net>
2013-11-15 21:19:46 +0900

    v0.0.1

--- lib/blogpost/livedoor.rb
+++ lib/blogpost/livedoor.rb
@@ -5,7 +5,7 @@ module Blogpost
       auth   = Atompub::Auth::Wsse.new(username: opts[:user],password: opts[:pass])
       client = Atompub::Client.new(auth: auth)
       entry  = Atom::Entry.new(title: opts[:title],content: opts[:content])
-      client.create_entry("http://livedoor.blogcms.jp/atom/", entry)
+      client.create_entry("http://cms.blog.livedoor.com/atom", entry)
     end
   end
 end
