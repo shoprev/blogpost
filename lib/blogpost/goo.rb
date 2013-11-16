f0e89fd718855804166c40508c81678eebed49e3
shoprev <admin@shoprev.net>
2013-11-16 16:28:14 +0900

    v0.0.1

--- lib/blogpost/goo.rb
+++ lib/blogpost/goo.rb
@@ -3,8 +3,7 @@ module Blogpost
     def self.post(opts={})
       opts = Blogpost.options.merge(opts)
       host="http://blog.goo.ne.jp/xmlrpc.php"
-      id = XMLRPC::Client.new2(host).call("blogger.getUsersBlogs", "",opts[:user], opts[:pass])[0]["blogid"]
-      XMLRPC::Client.new2(host).call("metaWeblog.newPost", id, opts[:user], opts[:pass],
+      XMLRPC::Client.new2(host).call("metaWeblog.newPost", "", opts[:user], opts[:pass],
         {"title" => opts[:title],"description" => opts[:content]}, true)
     end
   end
