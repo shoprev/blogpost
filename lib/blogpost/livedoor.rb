f6e7e5d69f876c48c212cdf5e2242dc7bb860155
shoprev <admin@shoprev.net>
2013-11-28 09:58:46 +0900

    v0.1.4

--- lib/blogpost/livedoor.rb
+++ lib/blogpost/livedoor.rb
@@ -4,7 +4,7 @@ module Blogpost
       opts = Blogpost.options.merge(opts)
       auth   = Atompub::Auth::Wsse.new(username: opts[:user],password: opts[:pass])
       client = Atompub::Client.new(auth: auth)
-      entry  = Atom::Entry.new(title: opts[:title],content: opts[:content])
+      entry  = Atom::Entry.new(title: opts[:title].encode("BINARY", "BINARY"),content: opts[:content].encode("BINARY", "BINARY"))
       service = client.get_service "http://livedoor.blogcms.jp/atom/"
       collection_uri = service.workspace.collections[0].href
       client.create_entry(collection_uri, entry)
