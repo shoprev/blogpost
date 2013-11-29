97ccb454a539f31c9f364530a1cbc7b84b217462
shoprev <admin@shoprev.net>
2013-11-29 21:14:34 +0900

    v0.1.6

--- lib/blogpost/hatenadiary.rb
+++ lib/blogpost/hatenadiary.rb
@@ -5,7 +5,9 @@ module Blogpost
       auth   = Atompub::Auth::Wsse.new(username: opts[:user],password: opts[:pass])
       client = Atompub::Client.new(auth: auth)
       entry  = Atom::Entry.new(title: opts[:title].encode('BINARY', 'BINARY'),content: opts[:content].encode('BINARY', 'BINARY'))
-      client.create_entry("http://d.hatena.ne.jp/#{opts[:user]}/atom", entry)
+      service = client.get_service "http://d.hatena.ne.jp/#{opts[:user]}/atom" #"http://d.hatena.ne.jp/%s/atom" % @user
+      collection_uri = service.workspace.collections[1].href
+      client.create_entry(collection_uri, entry)
     end
   end
 end
